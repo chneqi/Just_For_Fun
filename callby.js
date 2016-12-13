fabf96adde2769827e97cf1d56e3ebbcfa4e29dd
CQ <chneqi@users.noreply.github.com>
2016-12-14 00:53:16 +0800

    fix _

--- callby.js
+++ callby.js
@@ -7,15 +7,15 @@
 	};
 	var apply = function(fn){
 		return fn.apply(this,Array.from(this).concat(Array.prototype.slice.call(arguments,1)));
-	}
+	};
+	var _ = function(){
+		var f = arguments[arguments.length-1];
+		var value = this.valueOf();
+		arguments[0] = typeof value==='object'? this : value;
+		return f.apply(this,[this].concat(Array.prototype.slice(arguments,arguments.length-1)));
+	};
 	if(Object.defineProperty){
 		Object.defineProperty(Object.prototype,'callby',{
-			writable:true,
-			enumerable:false,
-			configurable:true,
-			value:call
-		});
-		Object.defineProperty(Object.prototype,'_',{
 			writable:true,
 			enumerable:false,
 			configurable:true,
@@ -27,9 +27,15 @@
 			configurable:true,
 			value:apply
 		});
+		Object.defineProperty(Object.prototype,'_',{
+			writable:true,
+			enumerable:false,
+			configurable:true,
+			value:_
+		});
 	}else{
 		Object.prototype.callby = call;
-		Object.prototype._ = call;
 		Object.prototype.applyby = apply;
+		Object.prototype._ = _;
 	}
 })()
