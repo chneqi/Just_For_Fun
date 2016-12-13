a9969041ad8231a8815a7d2f4987d7d9c3b9b3a6
CQ <chneqi@users.noreply.github.com>
2016-12-14 00:40:34 +0800

    add Object.defineProperty check and  alias

--- callby.js
+++ callby.js
@@ -1,21 +1,35 @@
-Object.defineProperty(Object.prototype,'callby',{
-	writable:true,
-	enumerable:false,
-	configurable:true,
-	value:function(fn){
-		// never handle if fn don't exist
+(function(){
+	var call = function(fn){
 		var f = fn;
 		var value = this.valueOf();
 		arguments[0] = typeof value==='object'? this : value;
 		return f.apply(this,arguments);
+	};
+	var apply = function(fn){
+		return fn.apply(this,Array.from(this).concat(Array.prototype.slice.call(arguments,1)));
 	}
-});
-Object.defineProperty(Object.prototype,'applyby',{
-	writable:true,
-	enumerable:false,
-	configurable:true,
-	value:function(fn){
-		var args = Array.prototype.slice.call(this).concat(Array.prototype.slice.call(arguments,1))
-		return fn.apply(this,args);
+	if(Object.defineProperty){
+		Object.defineProperty(Object.prototype,'callby',{
+			writable:true,
+			enumerable:false,
+			configurable:true,
+			value:call
+		});
+		Object.defineProperty(Object.prototype,'_',{
+			writable:true,
+			enumerable:false,
+			configurable:true,
+			value:call
+		});
+		Object.defineProperty(Object.prototype,'applyby',{
+			writable:true,
+			enumerable:false,
+			configurable:true,
+			value:apply
+		});
+	}else{
+		Object.prototype.callby = call;
+		Object.prototype._ = call;
+		Object.prototype.applyby = apply;
 	}
-});
+})()
